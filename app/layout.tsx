32f8c22116f3b337eba79892002acf0a7a07bd99
AkarExcel <therealakarexcel@gmail.com>
2023-07-27 06:55:50 -0500

    npm bulid not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ export const metadata: Metadata = {
   keywords: "Earn online, Make money, $5 daily"
 }
 
-const CrispWithNoSSR = dynamic(
+// const CrispWithNoSSR = dynamic(
   
-  () => import('./components/Crisp')
-)
+//   () => import('./components/Crisp')
+// )
 
 export default function RootLayout({
   children,
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <CrispWithNoSSR />
+      {/* <CrispWithNoSSR /> */}
       
       <body className={inter.className}>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 'use client'
-import Image from 'next/image'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import LiveWins from './components/LiveWins'
-import Guide from './components/Guide'
-import Why from './components/Why'
-import Testimonial from './components/Testimonial'
-import StartNow from './components/StartNow'
-import Footer from './components/Footer'
-import Emailist from './components/Emailist'
-import Footer2 from './components/Footer2'
+// import Header from './components/Header'
+// import Hero from './components/Hero'
+// import LiveWins from './components/LiveWins'
+// import Guide from './components/Guide'
+// import Why from './components/Why'
+// import Testimonial from './components/Testimonial'
+// import StartNow from './components/StartNow'
+// import Footer from './components/Footer'
+// import Emailist from './components/Emailist'
+// import Footer2 from './components/Footer2'
 
 
 export default function Home() {
   return (
   <>
     {/* <Header/> */}
-    <Emailist/>
+    {/* <Emailist/> */}
     {/* <main className='overflow-hidden mx-auto'>
       <Hero/>
       <LiveWins/>
